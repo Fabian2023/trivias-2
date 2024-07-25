9f24a6f235b3f0bed3e779b4e468551e0bea21d7
Fabian2023 <fasalpe34@gmail.com>
2024-07-25 16:09:49 -0500

    fix:

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorker from "../src/serviceWorker" // Asegúrate de que la ruta sea correcta
+import * as serviceWorker from "./serviceWorker" // Asegúrate de que la ruta sea correcta
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
